4761952ba80118be1cfe38eae564385cb482d6f2
Alex Wong <gmem@me.com>
2020-04-25 17:12:14 +0800

    mark outgoing packet with source ip 127.0.0.1

--- tools/istio-iptables/pkg/cmd/run.go
+++ tools/istio-iptables/pkg/cmd/run.go
@@ -425,6 +425,12 @@ func (iptConfigurator *IptablesConfigurator) run() {
 		iptConfigurator.handleInboundIpv6Rules(ipv6RangesExclude, ipv6RangesInclude)
 	}
 
+	if iptConfigurator.cfg.InboundInterceptionMode == constants.TPROXY {
+		// mark outgoing packets from 127.0.0.1/32 with 1337, match it to policy routing entry setup for TPROXY mode
+		iptConfigurator.iptables.AppendRuleV4(constants.OUTPUT, constants.MANGLE,
+			"-p", constants.TCP, "-s", "127.0.0.1/32", "!", "-d", "127.0.0.1/32",
+			"-j", constants.MARK, "--set-mark", iptConfigurator.cfg.InboundTProxyMark)
+	}
 	iptConfigurator.executeCommands()
 }
 
